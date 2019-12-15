8b8dd29a8230c0f4d54d7585428a46b1de3f88f5
neelkamath <neelkamath@icloud.com>
2019-12-15 14:40:01 +0530

    Test HTTP exceptions

--- src/test_main.py
+++ src/test_main.py
@@ -4,6 +4,7 @@ import main
 import starlette.testclient
 
 client = starlette.testclient.TestClient(main.app)
+
 ner_body = {
     'sections': [
         'Net income was $9.4 million compared to the prior year of $2.7 '
@@ -26,12 +27,22 @@ def test_ner_sense2vec_disabled():
         assert response.json() == json.load(f)
 
 
+def test_ner_fail():
+    fail('/ner', ner_body, 'ner')
+
+
+pos_body = {'text': 'Apple is looking at buying U.K. startup for $1 billion'}
+
+
 def test_pos():
-    text = {'text': 'Apple is looking at buying U.K. startup for $1 billion'}
-    response = client.post('/pos', json=text)
+    response = client.post('/pos', json=pos_body)
     assert response.status_code == 200
     with open('src/outputs/pos.json') as f:
         assert response.json() == json.load(f)
+
+
+def test_pos_fail():
+    fail('/pos', pos_body, 'parser')
 
 
 def test_tokenizer():
@@ -42,16 +53,29 @@ def test_tokenizer():
         assert response.json() == json.load(f)
 
 
+sentencizer_body = {
+    'text': 'Apple is looking at buying U.K. startup for $1 billion. Another '
+            + 'sentence.'
+}
+
+
 def test_sentencizer():
-    body = {
-        'text': 'Apple is looking at buying U.K. startup for $1 billion. '
-                + 'Another sentence.'
-    }
-    response = client.post('/sentencizer', json=body)
+    response = client.post('/sentencizer', json=sentencizer_body)
     assert response.status_code == 200
     with open('src/outputs/sentencizer.json') as f:
         assert response.json() == json.load(f)
 
 
+def test_sentencizer_fail():
+    fail('/sentencizer', sentencizer_body, 'parser')
+
+
 def test_health_check():
     assert client.get('/health_check').status_code == 204
+
+
+def fail(endpoint, body, pipe):
+    with main.nlp.disable_pipes(pipe):
+        response = client.post(endpoint, json=body)
+        assert response.status_code == 400
+        assert 'detail' in response.json()
