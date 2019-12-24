dc978dbefdee8cf5112da5142fd32772008fb364
neelkamath <neelkamath@icloud.com>
2019-12-24 08:59:33 +0530

    Fix #24

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,8 @@
 import json
 
+import fastapi
 import main
+import pytest
 import starlette.testclient
 
 client = starlette.testclient.TestClient(main.app)
@@ -34,6 +36,26 @@ def test_ner_spacy_fail():
 
 def test_ner_sense2vec_fail():
     fail('/ner', ner_sense2vec_body, 'sense2vec')
+
+
+def test_sense2vec_success():
+    body = {
+        'sentence': 'Bill Gates founded Microsoft in April 4, 1975.',
+        'phrase': 'Bill Gates'
+    }
+    response = client.post('/sense2vec', json=body)
+    assert response.status_code == 200
+    with open('src/outputs/sense2vec.json') as f:
+        assert response.json() == json.load(f)
+
+
+def test_sense2vec_fail():
+    response = client.post(
+        '/sense2vec',
+        json={'sentence': 'My name is John Doe.', 'phrase': 'Johnny Doe'}
+    )
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'phrase must be in sentence'
 
 
 pos_body = {'text': 'Apple is looking at buying U.K. startup for $1 billion'}
@@ -84,3 +106,23 @@ def fail(endpoint, body, pipe):
         response = client.post(endpoint, json=body)
         assert response.status_code == 400
         assert 'detail' in response.json()
+
+
+def test_enforce_components():
+    with pytest.raises(fastapi.HTTPException):
+        component = 'nonexistent_component'
+        main.enforce_components([component], component)
+
+
+def test_compute_phrases():
+    sentence = 'Bill Gates founded Microsoft in April 4, 1975.'
+    doc = main.nlp(sentence, disable=['tagger'])
+    for ent in list(doc.sents)[0].ents:
+        if ent.text == 'Bill Gates':
+            with open('src/outputs/compute_phrases.json') as f:
+                assert main.compute_phrases(ent) == json.load(f)
+
+
+def test_phrase_in_sentence():
+    with pytest.raises(fastapi.HTTPException):
+        main.PhraseInSentence(sentence='My name is John.', phrase='Johnny')
