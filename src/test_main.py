07aab687a21af12109e3b9c9f83ec0c6cd55a3aa
imsamuel <samuelywp@gmail.com>
2020-10-01 19:51:13 +0800

    chore: use direct imports for test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,19 +1,19 @@
 import json
 import re
-import typing
+from typing import List 
 
-import fastapi
-import main
-import pydantic
 import pytest
-import requests
-import starlette.testclient
+from fastapi import HTTPException
+from main import NERRequest, PhraseInSentence, TextModel, app, compute_phrases, enforce_components, nlp
+from pydantic import BaseModel
+from requests import Response
+from starlette.testclient import TestClient
 
-client: starlette.testclient.TestClient = starlette.testclient.TestClient(
-    main.app
+client: TestClient = TestClient(
+    app
 )
 
-ner_sections: typing.List[str] = [
+ner_sections: List[str] = [
     'Net income was $9.4 million compared to the prior year of $2.7 million. '
     + 'Google is a big company.',
     'Revenue exceeded twelve billion dollars, with a loss of $1b.'
@@ -21,9 +21,9 @@ ner_sections: typing.List[str] = [
 
 
 def test_ner_sense2vec_enabled() -> None:
-    response: requests.Response = client.post(
+    response: Response = client.post(
         '/ner',
-        json=dict(main.NERRequest(sections=ner_sections, sense2vec=True))
+        json=dict(NERRequest(sections=ner_sections, sense2vec=True))
     )
     assert response.status_code == 200
     with open('src/outputs/ner/sense2vec_enabled.json') as f:
@@ -31,44 +31,44 @@ def test_ner_sense2vec_enabled() -> None:
 
 
 def test_ner_sense2vec_disabled() -> None:
-    response: requests.Response = client.post(
+    response: Response = client.post(
         '/ner',
-        json=dict(main.NERRequest(sections=ner_sections))
+        json=dict(NERRequest(sections=ner_sections))
     )
     with open('src/outputs/ner/sense2vec_disabled.json') as f:
         assert response.json() == json.load(f)
 
 
 def test_ner_spacy_fail() -> None:
-    fail('/ner', main.NERRequest(sections=ner_sections), pipe='ner')
+    fail('/ner', NERRequest(sections=ner_sections), pipe='ner')
 
 
 def test_ner_sense2vec_fail() -> None:
     fail(
         '/ner',
-        main.NERRequest(sections=ner_sections, sense2vec=True),
+        NERRequest(sections=ner_sections, sense2vec=True),
         pipe='sense2vec'
     )
 
 
 def test_sense2vec_success() -> None:
-    body: main.PhraseInSentence = main.PhraseInSentence(
+    body: PhraseInSentence = PhraseInSentence(
         sentence='Bill Gates founded Microsoft in April 4, 1975.',
         phrase='Bill Gates'
     )
-    response: requests.Response = client.post('/sense2vec', json=dict(body))
+    response: Response = client.post('/sense2vec', json=dict(body))
     assert response.status_code == 200
     with open('src/outputs/sense2vec.json') as f:
         assert response.json() == json.load(f)
 
 
-pos_body: main.TextModel = main.TextModel(
+pos_body: TextModel = TextModel(
     text='Apple is looking at buying U.K. startup for $1 billion'
 )
 
 
 def test_pos() -> None:
-    response: requests.Response = client.post('/pos', json=dict(pos_body))
+    response: Response = client.post('/pos', json=dict(pos_body))
     assert response.status_code == 200
     with open('src/outputs/pos.json') as f:
         assert response.json() == json.load(f)
@@ -79,23 +79,23 @@ def test_pos_fail() -> None:
 
 
 def test_tokenizer() -> None:
-    text: main.TextModel = main.TextModel(
+    text: TextModel = TextModel(
         text='Apple is looking at buying U.K. startup for $1 billion'
     )
-    response: requests.Response = client.post('/tokenizer', json=dict(text))
+    response: Response = client.post('/tokenizer', json=dict(text))
     assert response.status_code == 200
     with open('src/outputs/tokenizer.json') as f:
         assert response.json() == json.load(f)
 
 
-sentencizer_body: main.TextModel = main.TextModel(
+sentencizer_body: TextModel = TextModel(
     text='Apple is looking at buying U.K. startup for $1 billion. Another '
          + 'sentence.'
 )
 
 
 def test_sentencizer() -> None:
-    response: requests.Response = client.post(
+    response: Response = client.post(
         '/sentencizer',
         json=dict(sentencizer_body)
     )
@@ -112,23 +112,23 @@ def test_health_check() -> None:
     assert client.get('/health_check').status_code == 204
 
 
-def fail(endpoint: str, body: pydantic.BaseModel, pipe: str) -> None:
-    with main.nlp.disable_pipes(pipe):
-        response: requests.Response = client.post(endpoint, json=dict(body))
+def fail(endpoint: str, body: BaseModel, pipe: str) -> None:
+    with nlp.disable_pipes(pipe):
+        response: Response = client.post(endpoint, json=dict(body))
         assert re.match(r'4\d\d', str(response.status_code))
         assert 'detail' in response.json()
 
 
 def test_enforce_components() -> None:
-    with pytest.raises(fastapi.HTTPException):
+    with pytest.raises(HTTPException):
         component: str = 'nonexistent_component'
-        main.enforce_components([component], component)
+        enforce_components([component], component)
 
 
 def test_compute_phrases() -> None:
     sentence: str = 'Bill Gates founded Microsoft in April 4, 1975.'
-    doc: main.nlp = main.nlp(sentence, disable=['tagger'])
+    doc: nlp = nlp(sentence, disable=['tagger'])
     for ent in list(doc.sents)[0].ents:
         if ent.text == 'Bill Gates':
             with open('src/outputs/compute_phrases.json') as f:
-                assert main.compute_phrases(ent) == json.load(f)
+                assert compute_phrases(ent) == json.load(f)
